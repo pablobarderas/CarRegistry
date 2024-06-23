e62ab0cfcd1c538c99385897adbd6028e74731c2
pablobarderas <pbarderas31@gmail.com>
2024-06-23 03:37:51 +0200

    Change pom dependencies and id generation of entities

--- src/main/java/com/bardev/CarRegistry/repository/entity/Brand.java
+++ src/main/java/com/bardev/CarRegistry/repository/entity/Brand.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class Brand {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
 
     private String name;
--- src/main/java/com/bardev/CarRegistry/repository/entity/CarEntity.java
+++ src/main/java/com/bardev/CarRegistry/repository/entity/CarEntity.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 public class CarEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @ManyToOne
